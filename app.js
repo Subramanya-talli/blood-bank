66c89152f9ec04b00741a46a9518391bfa7f306b
Subramanya Talli <subramanya.talli@gmail.com>
2024-10-23 21:01:44 +0530

    First-Commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const Dotenv = require("dotenv")
+Dotenv.config()
+const Express = require("express");
+const app = Express();
+const Path = require("path");
+const Port = process.env.PORT || 5000;
+
+app.use("/Assets/Images", Express.static(Path.join(__dirname, 'Assets/Images')));
+app.use("/Assets/CSS", Express.static(Path.join(__dirname, 'Assets/CSS')));
+
+
+app.set("view engine", "ejs");
+app.get("/", (req,res)=>
+{
+    return res.render("home");
+});
+
+app.listen(Port, ()=>{
+    console.log(`App is running on ${Port}`);
+});
+
